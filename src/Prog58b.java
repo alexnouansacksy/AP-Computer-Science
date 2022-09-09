dc8d9c112e8eb2985a22750e18da9d62d36fc36e
Alex Nouansacksy <nouansacks.a@stu.janesville.k12.wi.us>
2022-09-09 10:20:38 -0500

    Update

--- src/Prog58b.java
+++ src/Prog58b.java
@@ -1,6 +1,8 @@
 import java.util.*;
 
-public class Prog58b {    public static void main (String[] args) {
+public class Prog58b {
+    public static void main (String[] args) {
+
     Scanner keyboard = new Scanner(System.in);
 
     System.out.print("Enter A: ");
