c9807ba4a0ffe104b70f7e4f9d17ff3f1e88e3bd
alexnouansacksy <112714940+alexnouansacksy@users.noreply.github.com>
2022-09-13 18:53:35 -0500

    Update Prog58t.java

--- src/Prog58t.java
+++ src/Prog58t.java
@@ -1,1 +1,40 @@
+import java.util.*;
 
+class Main {
+  public static void main(String[] args) {
+    Scanner keyboard = new Scanner(System.in);
+
+    System.out.print("Enter Purchase Price: $");
+    double price = keyboard.nextDouble();
+
+    System.out.print("Enter Amount Received: $");
+    double received = keyboard.nextDouble();
+    
+    double changeDue = received - price;
+    double totalChange = changeDue;
+
+    double dollars = (((int)changeDue) * 100)/100 ;
+    changeDue -= dollars;
+    double quarters = (int) (changeDue / 0.25);
+    changeDue -= (quarters * 0.25);
+    double dimes = (int) (changeDue / 0.1);
+    changeDue -= (dimes * 0.10);
+    double nickels = (int) (changeDue / 0.05);
+    changeDue -= (nickels * 0.05);
+    double pennies = (int) (changeDue * 100);
+    
+
+    
+  
+
+      
+    System.out.printf("\nChange due: $%.2f", totalChange );
+    System.out.print("\n\nDollars: " + dollars);
+    System.out.print("\nQuarters: " + quarters);
+    System.out.print("\nDimes: " + dimes);
+    System.out.print("\nNickels: " + nickels);
+    System.out.print("\nPennies: " + pennies);
+    
+    
+  }
+}
