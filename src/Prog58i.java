cd62b1204abc5086c5fba0848db4c3dae9c04100
Alex Nouansacksy <nouansacks.a@stu.janesville.k12.wi.us>
2022-09-13 10:07:32 -0500

    Update

--- src/Prog58i.java
+++ src/Prog58i.java
@@ -7,13 +7,20 @@ public class Prog58i {
         Scanner input = new Scanner(System.in);
 
         System.out.println("The amount I wish to borrow is $");
-        double p = input.nextDouble();
+        double p = input.nextDouble(); // principal
 
         System.out.println("The loan rate is ");
-        double r = input.nextDouble();
+        double r = input.nextDouble(); // rate
 
         System.out.println("The number of months it will take me to pay off the loan is ");
-        double m = input.nextDouble();
+        double m = input.nextDouble(); // months
+
+        double mp = p * ((r/1200)* (Math.pow(m, (1+(r/1200)))
+                / (Math.pow(m, (1+(r/1200))) - 1)));
+
+        System.out.printf("My monthly payment will be $%.2f\n", mp);
+        System.out.printf("The total interest paid is $%.2f\n", mp * m);
+        System.out.printf("The total amount paid is $%.2f\n", ((mp*m) + p));
 
     }
 }
