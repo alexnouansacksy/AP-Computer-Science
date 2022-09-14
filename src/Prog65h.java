1b540a8cfe5a79721b39dc523f7ec23750b23f86
Alex Nouansacksy <nouansacks.a@stu.janesville.k12.wi.us>
2022-09-14 10:48:59 -0500

    Update

---
+++ src/Prog65h.java
@@ -0,0 +1,26 @@
+// Alex Nouansacksy
+// 09/14/2022
+// Prog65h
+
+import java.util.Scanner;
+
+public class Prog65h {
+    public static void main (String[] args) {
+        // £
+        Scanner keyboard = new Scanner((System.in));
+
+        System.out.print("Enter pounds: ");
+        double pounds = keyboard.nextDouble();
+
+        System.out.print("Enter shillings: ");
+        double shillings = keyboard.nextDouble();
+
+        System.out.print("Enter pence: ");
+        double pence = keyboard.nextDouble();
+
+        double total = pounds + (shillings / 20 + pence /12 / 20);
+
+        System.out.printf("Decimal pounds =" + total + " pounds");
+
+    }
+}
