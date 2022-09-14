e96e8a423467361a7900cf8fb737ad5a8f40c276
alexnouansacksy <112714940+alexnouansacksy@users.noreply.github.com>
2022-09-14 09:54:49 -0500

    Update Prog58h.java

--- src/Prog58h.java
+++ src/Prog58h.java
@@ -16,11 +16,24 @@ public class Prog58h {
         System.out.print("Number of days at interest: ");
         double t = keyboard.nextDouble();
 
-        double amount = p * (1 + (Math.pow(((n * t)/365), ((.01 * r )/ n))));
+       // double term1 = (.01 * r )/ n;
+       // double power = (n * t)/365;
+        // double amount = p * (1 + (Math.pow(term1, power)));
+  
+         double amount2 = p * Math.pow((1 + ((.01 * r) / n)), ((n * t) / 365));
 
-        double interest = amount - p;
+        double interest = amount2 - p;
+  
         System.out.printf("The interest earned is $%.2f", interest);
-        System.out.printf("\n The total amount in savings is now $%.2f", amount);
+        System.out.printf("\n The total amount in savings is now $%.2f", amount2);
     }
-}
-    
+  
+} 
+/*
+Amount Saved:5000
+Interest Rate: 11.5
+Number of times compounded per year: 365
+Number of days at interest: 900
+The interest earned is $1638.96
+The total amount in savings is now $6638.96
+*/
