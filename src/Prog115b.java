163f07cbf9e7c13fce8bcf7061ce428fa88cf1bf
Alex Nouansacksy <nouansacks.a@stu.janesville.k12.wi.us>
2022-09-19 10:06:56 -0500

    Update

---
+++ src/Prog115b.java
@@ -0,0 +1,16 @@
+// Alex Nouansacksy
+// 9/19/22
+// Prog115b
+
+public class Prog115b {
+    public static void main(String[] args) {
+        int lcv = 0; // loop control variable
+        while (lcv < 36){
+            lcv += 2;
+            System.out.print(lcv+ " ");
+        }
+    }
+}
+// 2 4 6 8 10 12 14 16 18 20 22 24 26 28 30 32 34 36
+
+
