8e1c36218daa1272bdc0dd424f5b71b752c0bda2
Alex Nouansacksy <nouansacks.a@stu.janesville.k12.wi.us>
2022-09-21 10:33:31 -0500

    Update

---
+++ src/Prog122d.java
@@ -0,0 +1,12 @@
+// Alex Nouansacksy
+// Prog122d
+// 09/20/22
+public class Prog122d {
+    public static void main(String[] args)  {
+        int lcv = -12;
+        while (lcv < 16) {
+            double x = lcv;
+            //double y = Math.pow(x, 6) - (3 * Math.pow(x, 5) -(93 * Math.pow(x, 4) + (87 * Math.pow())));
+        }
+    }
+}
