84f7fe2fd3a8b93bf4a2c2a41c6d0d6918a6e2bf
Alex Nouansacksy <nouansacks.a@stu.janesville.k12.wi.us>
2022-09-22 10:30:33 -0500

    Update

---
+++ src/Prog166f.java
@@ -0,0 +1,10 @@
+// Alex Nouansacksy
+// 09/22/22
+// Prog166f
+public class Prog166f {
+    public static void main(String[] args ) {
+        double a = 110;
+        double b = 0;
+        double c = 0;
+    }
+}
