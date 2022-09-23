dfd2b476e8ae2d594562b5140fb5287d108e8d89
alexnouansacksy <112714940+alexnouansacksy@users.noreply.github.com>
2022-09-23 12:10:58 -0500

    Update

--- src/Prog166f.java
+++ src/Prog166f.java
@@ -1,11 +1,21 @@
 // Alex Nouansacksy
 // 09/22/22
 // Prog166f
+
 public class Prog166f {
     public static void main(String[] args ) {
         double a = 100;
         double b = 0;
-        double c = 0;
+
+      while (a <= 110) {
+        while (b <= a) {
+          b++;
+          b += b;
+        }
+      System.out.println("The sum of all the numbers 1 to " + a + " is " + b);
+      b = 0;
+      a ++;
+      }
 
     }
 }
