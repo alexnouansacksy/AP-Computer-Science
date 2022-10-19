258621ce6e6fc757c0dfd642985c961b8c25c027
Alex Nouansacksy <nouansacks.a@stu.janesville.k12.wi.us>
2022-10-19 10:47:28 -0500

    Update

--- src/Prog76aClass.java
+++ src/Prog76aClass.java
@@ -5,6 +5,19 @@ import java.util.*;
 import static java.lang.System.out;
 public class Prog76aClass {
     public static void main(String[] args) {
+        out.print("Enter a number you dislike: ");
+
+        /*
+        System.out.print(num);
+        System.out.print("\nx 9");
+        System.out.print("\n---------");
+        System.out.print("\n" + product1);
+        System.out.print("\nx 12345679");
+        System.out.print("\n---------\n");
+        System.out.print(product2 + "  Surprise!");
+         */
+
+
 
     }
 }
