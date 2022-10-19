c7e03922a0431f894709469ee3d4443e09ea5ee0
Alex Nouansacksy <nouansacks.a@stu.janesville.k12.wi.us>
2022-10-19 10:46:40 -0500

    Update

--- src/Prog76aClass.java
+++ src/Prog76aClass.java
@@ -1,5 +1,10 @@
 // Alex Nouansacksy
 // Prog76aClass
 // 10/19/2022
+import java.util.*;
+import static java.lang.System.out;
 public class Prog76aClass {
+    public static void main(String[] args) {
+
+    }
 }
