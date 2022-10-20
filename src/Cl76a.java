1ddf70cfcc0e50bbd1d34257f374e6b012069158
Alex Nouansacksy <nouansacks.a@stu.janesville.k12.wi.us>
2022-10-20 09:53:06 -0500

    Update

--- src/Cl76a.java
+++ src/Cl76a.java
@@ -2,5 +2,20 @@
 // Prog76aCl
 // 10/19/2022
 public class Cl76a {
+    private int myNum;
+    private int myProduct1;
+    private int myProduct2;
+
+    public Cl76a (int num) {
+        myNum = num;
+    }
+
+    public void setStuff() {
+        myProduct1 = myNum * 9;
+        myProduct2 = myProduct1 * 12345679;
+    }
+
+    public int getMyProduct1() {return myProduct1;}
+    public int getMyProduct2() {return myProduct2;}
 
 }
--- src/Prog76aClass.java
+++ src/Prog76aClass.java
@@ -5,19 +5,22 @@ import java.util.*;
 import static java.lang.System.out;
 public class Prog76aClass {
     public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+
         out.print("Enter a number you dislike: ");
+        int num = keyboard.nextInt();
 
-        /*
-        System.out.print(num);
-        System.out.print("\nx 9");
-        System.out.print("\n---------");
-        System.out.print("\n" + product1);
-        System.out.print("\nx 12345679");
-        System.out.print("\n---------\n");
-        System.out.print(product2 + "  Surprise!");
-         */
+        Cl76a wow = new Cl76a(num);
+        wow.setStuff();
+        int product1 = wow.getMyProduct1();
+        int product2 = wow.getMyProduct2();
 
-
-
+        out.print(num);
+        out.print("\nx 9");
+        out.print("\n---------");
+        out.print("\n" + product1);
+        out.print("\nx 12345679");
+        out.print("\n---------\n");
+        out.print(product2 + "  Surprise!");
     }
 }
