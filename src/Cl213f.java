8ad8300cebf2f71cfe1dbaf0a124d8b7d836ab58
alexnouansacksy <112714940+alexnouansacksy@users.noreply.github.com>
2022-10-20 19:49:17 -0500

    Update Cl213f.java

--- src/Cl213f.java
+++ src/Cl213f.java
@@ -1,6 +1,26 @@
 // Alex Nouansacksy
 // Prog213fCl
-// 10/20/2022
+// 10/20/2022;
 public class Cl213f {
+    private int myHours;
+    private double myCost;
 
+    public Cl213f(int hours) {
+        myHours = hours;
+    }
+    public void setStuff () {
+        if (myHours >= 10000) {
+            myCost += (0.07 * 2000);
+            myCost += (0.05 * 8000);
+            myCost += (0.04 * (myHours - 10000));
+        }
+        else if (myHours >= 2000) {
+            myCost += (0.07 * 2000);
+            myCost += (0.05 * (myHours - 2000));
+        }
+        else if (myHours > 0) {
+            myCost += 0.07 * myHours;
+        }
+    }
+    public double getmyCost() {return myCost;}
 }
