55b66b5f961fb09e21a356dc931773ea8f56da98
alexnouansacksy <112714940+alexnouansacksy@users.noreply.github.com>
2022-10-20 19:49:32 -0500

    Update Prog213fClass.java

--- src/Prog213fClass.java
+++ src/Prog213fClass.java
@@ -8,12 +8,24 @@ public class Prog213fClass {
     public static void main(String[] args) {
         try {
             Scanner keyboard = new Scanner(new File("data/prog213f.dat"));
-
             while (keyboard.hasNext()) {
-                double num = keyboard.nextDouble();
+                int hours = keyboard.nextInt();
+                if (hours == -999) break;
+                Cl213f wow = new Cl213f(hours);
+                wow.setStuff();
+                double cost = wow.getmyCost();
+                out.printf("The cost of %d is $%.2f\n", hours, cost);
             }
+
         } catch (IOException e) {
             System.out.println("Can't find data file!");
         }
     }
 }
+/*
+The cost of 1338 is $93.66
+The cost of 9631 is $521.55
+The cost of 13561 is $682.44
+
+Process finished with exit code 0
+ */
