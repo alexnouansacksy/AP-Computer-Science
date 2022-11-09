f5628a8c9e6b02af49ab4f362809536a0277e791
Alex Nouansacksy <nouansacks.a@stu.janesville.k12.wi.us>
2022-11-09 10:47:55 -0600

    Update

--- src/Cl214b.java
+++ src/Cl214b.java
@@ -2,5 +2,47 @@
 // Prog214bCl
 // 11/07/2022
 public class Cl214b {
+    private int myId;
+    private double myYtd;
+    private double myRate;
+    private double myShiftCode;
+    private double myHours;
+    private double myWithholding;
+    private double myFica;
+    private double myNetPay;
+    private double myBasePay;
 
+    public Cl214b(int ID, double ytd, double rate, double shiftCode, double hours) {
+        myId = ID;
+        myYtd = ytd;
+        myRate = rate;
+        myShiftCode = shiftCode;
+        myHours = hours;
+        myWithholding = 0;
+        myFica = 0;
+        myNetPay = 0;
+    }
+
+    public void calc() {
+        double taxrate = 0;
+        myBasePay = myRate * myHours * myShiftCode;
+        if (myBasePay < 100) taxrate = 0;
+        else if (myBasePay > 100 && myBasePay < 150) taxrate = 0.08;
+        else if (myBasePay > 150 && myBasePay < 200) taxrate = 0.12;
+        else if (myBasePay > 200 && myBasePay < 300) taxrate = 0.15;
+        else if (myBasePay >= 300) taxrate = 17;
+        myWithholding = myBasePay * taxrate;
+
+        myFica = 0;
+        if (myYtd > 17300) myFica = 0;
+        else if (myYtd + myBasePay <= 17300) myFica = myBasePay * 0.0605;
+        else if (myYtd < 17300 && (myYtd + myBasePay > 17300)) {
+            myFica = 0.0605 * ((myYtd + myBasePay) - 17300);
+        }
+        myNetPay = (myBasePay - myWithholding) - myFica;
+    }
+
+    public String toString() { return "Employee Number: " + myId + "\tHours: " + myHours + "\tShift Factor: " + myShiftCode
+            + "\tGross Pay: " + myBasePay + "\tYear-to date: " + myYtd + "\nWithholding: " + myWithholding + "\tFICA: "
+    + myFica + "\tNet Pay: " + myNetPay;}
 }
--- src/Prog214b.java
+++ src/Prog214b.java
@@ -2,6 +2,8 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
 
+import static java.lang.System.out;
+
 // Alex Nouansacksy
 // Prog214b
 // 11/07/2022
@@ -9,6 +11,27 @@ public class Prog214b {
     public static void main(String[] args) {
         try {
             Scanner keyboard = new Scanner(new File("data/prog214b.dat"));
+
+            Cl214b[] list = new Cl214b[1000];
+            int cnt = 0;
+
+            while (keyboard.hasNext()) {
+                int ID = keyboard.nextInt();
+                double ytd = keyboard.nextDouble();
+                double rate = keyboard.nextDouble();
+                double shiftCode = keyboard.nextDouble();
+                double hours = keyboard.nextDouble();
+
+                Cl214b wow = new Cl214b(ID, ytd, rate, shiftCode, hours);
+                list[cnt] = wow;
+                cnt += 5;
+            }
+
+            for (int lcv = 0; lcv < cnt; lcv += 5)
+                list[lcv].calc();
+
+            for (int lcv = 0; lcv < cnt; lcv += 5)
+                out.println(list[lcv].toString());
 
         } catch (IOException e) {
             System.out.println("Can't find data file!");
