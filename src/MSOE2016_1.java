49888120a1bc7ca43df0da2ea6978b5ff78fbac5
Alex Nouansacksy <nouansacks.a@stu.janesville.k12.wi.us>
2022-11-15 10:30:29 -0600

    Update

---
+++ src/MSOE2016_1.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import static java.lang.System.out;
+public class MSOE2016_1 {
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        out.print("Enter a lowercase word");
+        String phrase = keyboard.nextLine();
+        String[] word = new String[phrase.length()];
+        for (int lcv = 0; lcv < word.length; lcv++) {
+
+        }
+    }
+
+
+}
