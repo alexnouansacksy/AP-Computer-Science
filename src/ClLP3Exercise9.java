84cd1ec16b01db344030b6c029cbb32230d8ad43
Alex Nouansacksy <nouansacks.a@stu.janesville.k12.wi.us>
2022-11-21 10:30:23 -0600

    Update

---
+++ src/ClLP3Exercise9.java
@@ -0,0 +1,9 @@
+// Alex Nouansacksy
+// LP3Exericse9Cl
+// 11/21/2022
+public class ClLP3Exercise9 {
+    private int myBirthYear;
+    private int myBirthMonth;
+    private int myBirthDay;
+    private int myCurrentYear;
+}
--- src/LP3Exercise9Class.java
+++ src/LP3Exercise9Class.java
@@ -1,8 +1,27 @@
 // Alex Nouansacksy
 // LP3Exercise9Class
 // 11/16/2022
+import static java.lang.System.out;
+import java.util.*;
 public class LP3Exercise9Class {
     public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        out.println("Enter Your Birthdate: ");
+        out.print("Year: ");
+        int birthYear = keyboard.nextInt();
+        out.print("Month: ");
+        int birthMonth = keyboard.nextInt();
+        out.print("Day: ");
+        int birthDay = keyboard.nextInt();;
+        out.println("Enter Today's Date: ");
+        out.print("Year: ");
+        int currentYear = keyboard.nextInt();
+        out.print("Month: ");
+        int currentMonth = keyboard.nextInt();
+        out.print("Day: ");
+        int currentDay = keyboard.nextInt();
+
+
 
     }
 }
