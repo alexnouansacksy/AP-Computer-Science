dd6e57f166f97e5e6c54476f07683664515a5b48
Alex Nouansacksy <nouansacks.a@stu.janesville.k12.wi.us>
2022-11-22 10:11:25 -0600

    Update

--- src/ClLP3Exercise9.java
+++ src/ClLP3Exercise9.java
@@ -6,4 +6,37 @@ public class ClLP3Exercise9 {
     private int myBirthMonth;
     private int myBirthDay;
     private int myCurrentYear;
+    private int myCurrentMonth;
+    private int myCurrentDay;
+
+    private int myDaysAtBirth;
+    private int myTotalDaysNow;
+    private int myDaysAlive;
+    private int myHoursSlept;
+
+    public ClLP3Exercise9 (int birthYear, int birthMonth, int birthDay, int currentYear, int currentMonth, int currentDay) {
+        myBirthYear = birthYear;
+        myBirthMonth = birthMonth;
+        myBirthDay = birthDay;
+        myCurrentYear = currentYear;
+        myCurrentMonth = currentMonth;
+        myCurrentDay = currentDay;
+        myDaysAtBirth = 0;
+        myTotalDaysNow = 0;
+    }
+
+    public void setStuff() {
+        myDaysAlive = (365 * myCurrentYear + 30 * myCurrentMonth + myCurrentDay) -
+                (365 * myBirthYear + 30 * myBirthMonth + myBirthDay);
+        myHoursSlept = (myDaysAlive * 8);
+    }
+
+    public String toString() {return String.format("You have been alive for %d days.\nYou have slept for %d hours",
+            myDaysAlive, myHoursSlept);}
+
+    public void print() {
+        System.out.println(this.toString());
+    }
+
 }
+
--- src/LP3Exercise9Class.java
+++ src/LP3Exercise9Class.java
@@ -21,7 +21,24 @@ public class LP3Exercise9Class {
         out.print("Day: ");
         int currentDay = keyboard.nextInt();
 
-
+        ClLP3Exercise9 wow = new ClLP3Exercise9(birthYear, birthMonth, birthDay, currentYear, currentMonth, currentDay);
+        wow.setStuff();
+        wow.print();
 
     }
 }
+/*
+Enter Your Birthdate:
+Year: 1997
+Month: 2
+Day: 12
+Enter Today's Date:
+Year: 2012
+Month: 08
+Day: 03
+You have been alive for 5646 days.
+You have slept for 45168 hours
+
+Process finished with exit code 0
+
+ */
