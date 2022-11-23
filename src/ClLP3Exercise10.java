4087bb38814b19a62b60f5460929a2dbdc6dee78
alexnouansacksy <112714940+alexnouansacksy@users.noreply.github.com>
2022-11-23 08:22:29 -0600

    Update ClLP3Exercise10.java

--- src/ClLP3Exercise10.java
+++ src/ClLP3Exercise10.java
@@ -8,26 +8,61 @@ public class ClLP3Exercise10 {
     double myFriesCost;
     int mySodas;
     double mySodasCost;
+    double cost;
     double tax;
     double totalcost;
-
-    public ClLP3Exercise10 (int burgers, int fries, int sodas) {
+    double change;
+    double cashGiven;
+  
+    public ClLP3Exercise10 (int burgers, int fries, int sodas, double cash) {
         myBurgers = burgers;
         myFries = fries;
         mySodas = sodas;
+        myBurgerCost = 0;
+        myFriesCost = 0;
+        mySodasCost = 0;
+        cost = 0;
+        tax = 0;
+        totalcost = 0;
+        cashGiven = cash;
+        change = 0;
     }
 
     public void setStuff() {
         myBurgerCost = myBurgers * 1.69;
         myFriesCost = myFries * 1.09;
         mySodasCost = mySodas * 0.99;
-
-        tax = 0.065 * (myBurgerCost + myFriesCost + mySodasCost);
-        totalcost = (myBurgerCost + myFriesCost + mySodasCost) + tax;
+        cost = (myBurgerCost + myFriesCost + mySodasCost);
+      
+        tax = 0.065 * cost;
+        totalcost = cost + tax;
+        
+        change = cashGiven - totalcost;
     }
 
-    public String toString() {return String.format("Total before tax: ");}
-    //public getChange(int tendered) {
+    public String toString() {return String.format("Total before tax: $%.2f\nTax: $%.2f\nTotal: $%.2f\nChange: $%.2f", cost, tax, totalcost, change);}
 
-    //}
+    public void print() {
+        System.out.println(this.toString());
+    }
 }
+
+/*
+PART A
+Enter the number of burgers: 2
+ENter the number of fries: 5
+Enter the number of sodas: 5
+Total before tax: $13.78
+Tax: $0.90
+Total: $14.68
+
+PART B
+Enter the number of burgers: 2
+ENter the number of fries: 5
+Enter the number of sodas: 5
+Total before tax: $13.78
+Cash Tendered: $20
+Tax: $0.90
+Total: $14.68
+Change: $5.32
+*/
