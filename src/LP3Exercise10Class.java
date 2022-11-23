d66ce52f55961731c9cc456c1176e5eb6e295b38
alexnouansacksy <112714940+alexnouansacksy@users.noreply.github.com>
2022-11-23 08:24:06 -0600

    Update LP3Exercise10Class.java

--- src/LP3Exercise10Class.java
+++ src/LP3Exercise10Class.java
@@ -12,8 +12,32 @@ public class LP3Exercise10Class {
             int fries = keyboard.nextInt();
             out.print("Enter the number of sodas: ");
             int sodas = keyboard.nextInt();
+            out.print("Cash Tendered: ");
+            double tendered = keyboard.nextDouble();
 
-            ClLP3Exercise10 wow = new ClLP3Exercise10(burgers, fries, sodas);
+            ClLP3Exercise10 wow = new ClLP3Exercise10(burgers, fries, sodas, tendered);
 
+            wow.setStuff();
+            wow.print();
     }
 }
+
+/*
+PART A
+Enter the number of burgers: 2
+Enter the number of fries: 5
+Enter the number of sodas: 5
+Total before tax: $13.78
+Tax: $0.90
+Total: $14.68
+
+PART B
+Enter the number of burgers: 2
+Enter the number of fries: 5
+Enter the number of sodas: 5
+Total before tax: $13.78
+Cash Tendered: $20
+Tax: $0.90
+Total: $14.68
+Change: $5.32
+*/
