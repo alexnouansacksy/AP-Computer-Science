dd4a73947705c0f0a904fa800fc77e19b6e6faea
Alex Nouansacksy <nouansacks.a@stu.janesville.k12.wi.us>
2022-12-02 10:31:25 -0600

    Update

--- src/Cl214c.java
+++ src/Cl214c.java
@@ -1,3 +1,48 @@
 public class Cl214c {
+    private String myGasType;
+    private double myGallons;
+    private String myCarWash;
+    private double myPerGallon;
+
+    private double myGasCost;
+    private double myWashCost;
+    private double myGrandTotal;
+
+    public Cl214c(String gasType, double gallons, String carWash) {
+        myGasType = gasType;
+        myGallons = gallons;
+        myCarWash = carWash;
+        myGasCost = 0;
+        myWashCost = 0;
+        myGrandTotal = 0;
+        myPerGallon = 0;
+    }
+
+    public void setCosts() {
+        if (myGasType.equals("R")) {
+            myGasType = "Regular";
+            myPerGallon = 1.359;
+        }
+        if (myGasType.equals("P")) {
+            myGasType = "Premium";
+            myPerGallon = 1.479;
+        }
+        if (myGasType.equals("H")) {
+            myGasType = "High Octane";
+            myPerGallon = 1.429;
+        }
+
+        myGasCost = myGallons * myPerGallon;
+        if (myCarWash.equals("Y")) myWashCost = 2;
+        myGrandTotal = myGasCost + myWashCost;
+    }
+
+    public double getPerGallon() {return myPerGallon;}
+    public double getGasCost() {return myGasCost;}
+    public double getWashCost() {return myWashCost;}
+    public double getGrandTotal() {return myGrandTotal;}
+    public String getGasType() {return myGasType;}
+    public double getGallons() {return myGallons;}
+    public String getWash() {return myCarWash;}
 
 }
--- src/Prog214cArrayList.java
+++ src/Prog214cArrayList.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Scanner;
 import static java.lang.System.out;
 // Alex Nouansacksy
@@ -10,9 +11,82 @@ public class Prog214cArrayList {
     public static void main(String[] args) {
         try {
             Scanner keyboard = new Scanner(new File("data/prog214c.dat"));
+            ArrayList<Cl214c> list = new ArrayList<Cl214c>();
+
+            while (keyboard.hasNext()) {
+                String gasType = keyboard.next();
+                double gallons = keyboard.nextDouble();
+                String carWash = keyboard.next();
+                Cl214c wow = new Cl214c(gasType, gallons, carWash);
+                list.add(wow);
+
+                for (int lcv = 0; lcv < list.size(); lcv++) {
+                    Cl214c current = list.get(lcv);
+                    current.setCosts();
+                }
+
+
+            }
+
+            for (Cl214c me : list) {
+                out.println("COMPSCI PETROLEUM COMPANY");
+                out.println("----------------------------------------");
+                out.println(me.getGasType() + "\t\t\t\t" + me.getGallons() + " gallons @ " + me.getPerGallon());
+                out.println("----------------------------------------");
+                out.println("Gasoline\t\t\t\t\t\t\t" + String.format("%.2f", me.getGasCost()));
+                out.println("Wash " + me.getWash() + "\t\t\t\t\t\t\t\t" + String.format("%.2f", me.getWashCost()));
+                out.println("\t\t\t\t\t\t\t\t\t------");
+                out.println(("Total Due\t\t\t\t\t\t\t" + String.format("%.2f", me.getGrandTotal())));
+                out.println("----------------------------------------");
+                out.println("");
+            }
 
         } catch (IOException e) {
             out.println("Can't find data file!");
         }
     }
 }
+/*
+----------------------------------------
+Premium				15.0 gallons @ 1.479
+----------------------------------------
+Gasoline							22.19
+Wash N								0.00
+									------
+Total Due							22.19
+----------------------------------------
+
+COMPSCI PETROLEUM COMPANY
+----------------------------------------
+Regular				21.4 gallons @ 1.359
+----------------------------------------
+Gasoline							29.08
+Wash Y								2.00
+									------
+Total Due							31.08
+----------------------------------------
+
+COMPSCI PETROLEUM COMPANY
+----------------------------------------
+High Octane				7.5 gallons @ 1.429
+----------------------------------------
+Gasoline							10.72
+Wash Y								2.00
+									------
+Total Due							12.72
+----------------------------------------
+
+COMPSCI PETROLEUM COMPANY
+----------------------------------------
+Regular				17.9 gallons @ 1.359
+----------------------------------------
+Gasoline							24.33
+Wash Y								2.00
+									------
+Total Due							26.33
+----------------------------------------
+
+
+Process finished with exit code 0
+
+ */
