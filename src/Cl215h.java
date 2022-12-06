07e279ff724f1865ec75f84abce84837d66ec687
Alex Nouansacksy <nouansacks.a@stu.janesville.k12.wi.us>
2022-12-06 10:10:59 -0600

    Update

--- src/Cl215h.java
+++ src/Cl215h.java
@@ -1,2 +1,33 @@
+// Alex Nouansacksy
+// Prog215hCl
+// 12/06/2022
 public class Cl215h {
+    private double myNum1;
+    private double myNum2;
+    private double myNum3;
+    private double myNum4;
+    private double myNum5;
+    private double myNum6;
+    private double myNum7;
+    private double myNum8;
+
+    private double myAverage;
+
+    public Cl215h (double num1, double num2, double num3, double num4, double num5, double num6, double num7, double num8) {
+        myNum1 = num1;
+        myNum2 = num2;
+        myNum3 = num3;
+        myNum4 = num4;
+        myNum5 = num5;
+        myNum6 = num6;
+        myNum7 = num7;
+        myNum8 = num8;
+        myAverage = 0;
+    }
+
+    public void setStuff() {
+        myAverage = (myNum1 + myNum2 + myNum3 + myNum4 + myNum5 + myNum6 + myNum7 + myNum8) / (double)8;
+    }
+
+    public double getAverage() {return myAverage;}
 }
--- src/Prog215h.java
+++ src/Prog215h.java
@@ -11,12 +11,28 @@ import static java.lang.System.out;
 public class Prog215h {
     public static void main(String[] args) {
         try {
-            Scanner keyboard = new Scanner(new File("data/prog215h.dat"));
+            Scanner keyboard = new Scanner(new File("data/prog215h.txt"));
             ArrayList<Double> wow = new ArrayList<Double>();
             int cnt = 0;
             while (keyboard.hasNext()) {
-                //wow[cnt] = keyboard.nextDouble();
+                wow.add(keyboard.nextDouble());
+            }
+            int participants = wow.size() / 3;
+            for (int lcv = 0; lcv <= participants; lcv += 8) {
+                cnt++;
+                double num1 = wow.get(lcv);
+                double num2 = wow.get(lcv + 1);
+                double num3 = wow.get(lcv + 2);
+                double num4 = wow.get(lcv + 3);
+                double num5 = wow.get(lcv + 4);
+                double num6 = wow.get(lcv + 5);
+                double num7 = wow.get(lcv + 6);
+                double num8 = wow.get(lcv + 7);
 
+                Cl215h alex = new Cl215h(num1, num2, num3, num4, num5, num6, num7, num8);
+                alex.setStuff();
+                out.printf("Contestant %d\n %.1f %.1f %.1f %.1f %.1f %.1f %.1f %.1f = %.2f\n", cnt, num1, num2, num3, num4, num5, num6,
+                        num7, num8, alex.getAverage());
             }
 
         } catch (IOException e) {
@@ -24,3 +40,11 @@ public class Prog215h {
         }
     }
 }
+/*
+Contestant 1
+ 8.4 9.1 8.5 8.4 9.1 8.7 8.8 9.1 = 8.76
+Contestant 2
+ 7.0 7.0 7.0 7.0 7.0 7.0 7.0 7.0 = 7.00
+Contestant 3
+ 8.0 7.9 8.0 8.0 8.0 8.0 8.0 8.1 = 8.00
+ */
