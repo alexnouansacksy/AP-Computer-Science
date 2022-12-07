101f908606ef8645700bea0df3c1bfafc7fe76b6
alex <nouansacks.a@stu.janesville.k12.wi.us>
2022-12-06 22:23:05 -0600

    Update (Desktop)

--- src/Prog215i.java
+++ src/Prog215i.java
@@ -10,25 +10,26 @@ public class Prog215i {
     public static void main(String[] args) {
         try {
             Scanner keyboard = new Scanner(new File("data/prog215i.dat"));
-            ArrayList<Cl215i> wow = new ArrayList<Cl215i>();
+            ArrayList<Cl215i> list = new ArrayList<Cl215i>();
 
             while (keyboard.hasNext()) {
-                if(keyboard.nextDouble() != 0){
-                    int vehicle = keyboard.nextInt();
-                    double gallons = keyboard.nextDouble();
-                    double miles = keyboard.nextDouble();
-                    Cl215i alex = new Cl215i(vehicle, gallons, miles);
-                    wow.add(alex);
-                }
-                for (int lcv = 0; lcv < wow.size(); lcv++) {
-                    Cl215i current = wow.get(lcv);
+                int vehicle = keyboard.nextInt();
+                double gallons = keyboard.nextDouble();
+                double miles = keyboard.nextDouble();
+                Cl215i wow = new Cl215i(vehicle, gallons, miles);
+                list.add(wow);
+
+                for (int lcv = 0; lcv < list.size(); lcv++) {
+                    Cl215i current = list.get(lcv);
                     current.setStuff();
                 }
 
-                for (Cl215i me: wow) {
-                    out.printf("%d\t%.1f\t%.1f\t%.1f\n", me.getVehicleNum(), me.getMiles(), me.getGallons(), me.getMPG());
-                }
             }
+            for (Cl215i me: list) {
+                out.printf("%d\t%.1f\t%.1f\t%.1f\n", me.getVehicleNum(), me.getMiles(), me.getGallons(), me.getMPG());
+            }
+
+
         } catch (IOException e) {
             out.println("Can't find data file!");
         }
