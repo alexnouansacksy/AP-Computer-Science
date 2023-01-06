6afd101569060a5e9fe316ddfe7fdcef922d686f
Alex Nouansacksy <nouansacks.a@stu.janesville.k12.wi.us>
2023-01-06 09:57:43 -0600

    Update

--- src/Prog465c.java
+++ src/Prog465c.java
@@ -11,14 +11,44 @@ public class Prog465c {
     public static void main(String[] args) {
         try {
             Scanner keyboard = new Scanner(new File("data/prog465c.txt"));
-            Scanner input = new Scanner(System.in);
-            out.print("Rows: ");
-            int rows = input.nextInt();
-            out.print("Columns: ");
-            int columns = input.nextInt();
+            int rows = keyboard.nextInt();
+            int columns = keyboard.nextInt();
+
+            int[][] mat = new int[rows][columns];
+
+            for(int row = 0; row < mat.length; row++) {
+                for (int col = 0; col < mat[0].length; col++) {
+                    mat[row][col] = keyboard.nextInt();
+                }
+            }
+
+            for (int[] row : mat) {
+                for (int num:row) out.print(num + "\t");
+                out.println();
+            }
+
+            int tot = 0;
+            for (int i = 0; i < columns; i++) {
+                tot += mat[0][i];
+                tot += mat[rows - 1][i];
+            }
+            for (int i = 1; i < rows - 1; i++) {
+                tot += mat[i][0];
+                tot += mat[i][columns - 1];
+            }
+            out.println("The total of the edges is " + tot);
 
         } catch (IOException e) {
             out.println("Can't find data file!");
         }
     }
 }
+/*
+100	195	182	225
+83	125	235	67
+129	42	100	750
+The total of the edges is 1873
+
+Process finished with exit code 0
+
+ */
