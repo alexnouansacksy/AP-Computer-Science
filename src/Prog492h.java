6994d5bd9cd416629bc5fb75b72d1f0f9de9c779
Alex Nouansacksy <nouansacks.a@stu.janesville.k12.wi.us>
2023-01-27 10:30:53 -0600

    Update

--- src/Prog492h.java
+++ src/Prog492h.java
@@ -23,16 +23,59 @@ public class Prog492h {
                 }
             }
 
-            for (int[] row : mat) {
-                for (int n : row) {
-                    if(n == 1) out.print("*");
-                    else out.print(" ");
+
+            for (int i = 0; i < 15; i++) {
+
+                for (int[] row : mat) {
+                    for (int n : row) {
+                        if(n == 1) out.print("*");
+                        else out.print(" ");
+                    }
+                    out.println();
                 }
-                out.println();
+
+                int[][] temp = new int[rows][cols];
+
+                for (int row = 0; row < mat.length; row ++) {
+                    for (int col = 0; col < mat[0].length; col++) {
+                        temp[row][col] = mat[row][col];
+                    }
+                }
+
+
+                int neighbors = 0;
+
+                for (int row = 0; row < mat.length; row ++) {
+                    for (int col = 0; col < mat[0].length; col++) {
+                        if (row - 1 < 0 || col - 1 < 0) neighbors = neighbors;
+                        else {
+                            if (mat[row - 1][col - 1] == 1) neighbors++;
+                            if (mat[row - 1][col] == 1) neighbors++;
+                            if (mat[row - 1][col + 1] == 1) neighbors++;
+                            if (mat[row][col - 1] == 1) neighbors++;
+                            if (mat[row][col + 1] == 1) neighbors++;
+                            if (mat[row + 1][col - 1] == 1) neighbors++;
+                            if (mat[row + 1][col] == 1) neighbors++;
+                            if (mat[row + 1][col + 1] == 1) neighbors++;
+                        }
+
+                        if (neighbors == 3 && mat[row][col] == 0) {
+                            temp[row][col] = 1;
+                        } else if (neighbors >= 4 && mat[row][col] == 1) {
+                            temp[row][col] = 0;
+                        }
+                    }
+                }
+
             }
+
+
+
 
         } catch (IOException e) {
             out.println("Can't find data file!");
         }
+
+
     }
 }
