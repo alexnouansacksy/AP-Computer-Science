af0928a6e7cb7d2e200877af2c194548d43d8aba
Alex Nouansacksy <nouansacks.a@stu.janesville.k12.wi.us>
2023-02-13 10:19:31 -0600

    Update

--- src/Prog408a.java
+++ src/Prog408a.java
@@ -1,2 +1,32 @@
+// Alex Nouansacksy
+// Prog408a
+// 02/13/2023
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+import static java.lang.System.out;
+
 public class Prog408a {
+    public static void main(String[] args) {
+        try {
+            Scanner keyboard = new Scanner(new File("data/prog285b.dat"));
+            Cl408a[] list = new Cl408a[21];
+            int cnt = 0;
+            while (keyboard.hasNext()) {
+                int id = keyboard.nextInt();
+                int score = keyboard.nextInt();
+                list[cnt] = new Cl408a(id, score);
+                cnt++;
+            }
+
+            list = SortingAlgs.insertionSort(list);
+            out.println("Id\tScore");
+            for(int i = 0; i < list.length; i++) {
+                out.println(list[i].getID() + "\t" + list[i].getScore());
+            }
+        } catch (IOException e) {
+            out.println("Can't find data file!");
+        }
+    }
 }
--- src/SortingAlgs.java
+++ src/SortingAlgs.java
@@ -1,11 +1,11 @@
 public class SortingAlgs {
     public static int[] bubbleSort(int[] array) {
-        for (int lcv = 0; lcv < array.length; lcv++) {
-            for (int lcv2 = 0; lcv2 < array.length - 1; lcv2++) {
-                if (array[lcv2] > array[lcv2 + 1]) {
-                    int temp = array[lcv2];
-                    array[lcv2] = array[lcv2 + 1];
-                    array[lcv2 + 1] = temp;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
                 }
             }
         }
@@ -13,6 +13,41 @@ public class SortingAlgs {
     }
 
     public static int[] selectionSort(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            int min = i;
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[j] < array[min]) min = j;
+            }
+            int temp = array[i];
+            array [i] = array[min];
+            array[min] = temp;
+        }
+        return array;
+    }
+
+    public static int[] insertionSort(int[] array) {
+        for(int i = 1; i < array.length; i++) {
+            int temp = array[i];
+            int j = i - 1;
+            while (j >= 0 && array[j] > temp) {
+                array[j + 1] = array[j];
+                j--;
+            }
+            array[j +1] = temp;
+        }
+        return array;
+    }
+
+    public static Cl408a[] insertionSort(Cl408a[] array) {
+        for(int i = 1; i < array.length; i++) {
+            Cl408a temp = array[i];
+            int j = i - 1;
+            while (j >= 0 && array[j].getScore() < temp.getScore()) {
+                array[j + 1] = array[j];
+                j--;
+            }
+            array[j +1] = temp;
+        }
         return array;
     }
 }
