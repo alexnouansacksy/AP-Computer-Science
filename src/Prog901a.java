1525f91c658454e351afce6d691d272d978b8236
alex <nouansacks.a@stu.janesville.k12.wi.us>
2023-02-16 14:49:06 -0600

    Update (Desktop)

--- src/Prog901a.java
+++ src/Prog901a.java
@@ -1,3 +1,21 @@
-public class Prog901a {
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
 
+import static java.lang.System.out;
+
+// Alex Nouansacksy
+// Prog901a
+// 02/16/2023
+public class Prog901a {
+    public static void main(String[] args) {
+        try {
+            Scanner keyboard = new Scanner(new File("data/prog512h.dat"));
+            String phrase = "";
+            while (keyboard.hasNext()) phrase = phrase + keyboard.next() + " ";
+            out.print(phrase);
+        } catch (IOException e) {
+            out.println("Can't find data file!");
+        }
+    }
 }
