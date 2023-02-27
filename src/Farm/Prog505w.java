883b98f83771f99ae88eb019ca8a2f5251c33557
Alex Nouansacksy <nouansacks.a@stu.janesville.k12.wi.us>
2023-02-27 10:33:57 -0600

    Update

--- src/Farm/Prog505w.java
+++ src/Farm/Prog505w.java
@@ -50,8 +50,57 @@ public class Prog505w {
                 }
             }
 
-            // ...  everything else
+            // income for the day
 
+            double totIncome = 0;
+            for (Animal animal : animals) {
+                totIncome += animal.value(cornCost, hayCost);
+            }
+            out.println("Total income for today is $" + totIncome);
+
+            // cumulative weight
+
+            double totWeight = 0;
+            for (Animal animal : animals) {
+                totWeight += animal.getWeight();
+            }
+            out.println("Culumative weight is " + totWeight);
+
+            // enough food to feed the animals
+
+            double cornEaten = 0;
+            double hayEaten = 0;
+
+            for(Animal animal : animals) {
+                cornEaten += animal.getNumCorn();
+                hayEaten += animal.getNumHayBales();
+            }
+
+            if (hayEaten < hay) out.println("There is enough hay.");
+            else out.println("There is not enough hay.");
+
+            if (cornEaten < corn) out.println("There is enough corn.");
+            else out.println("There is not enough corn.");
+
+            // cow that makes the most money
+            double maxCow = 0;
+            int maxCowIndex = 0;
+            for (int i = 0; i < animals.size(); i++) {
+                if (animals.get(i) instanceof Cow) {
+                    Cow cow = (Cow) animals.get(i);
+                    if (cow.value(cornCost, hayCost) > maxCow) {
+                        maxCow = cow.value(cornCost, hayCost);
+                        maxCowIndex = i;
+                    }
+                }
+            }
+
+            out.printf("Cow %s makes the most money\n", animals.get(maxCowIndex).getName());
+
+
+
+
+            // horse with least money
             double minHorse = Double.MAX_VALUE;
             int minHorseIndex = 0;
             for (int lcv = 0; lcv < animals.size(); lcv++) {
@@ -72,3 +121,13 @@ public class Prog505w {
         }
     }
 }
+/*
+Total income for today is $213.5
+Culumative weight is 32790.0
+There is enough hay.
+There is enough corn.
+Cow Barb makes the most money
+Horse JohnJohn makes the least money
+
+Process finished with exit code 0
+ */
