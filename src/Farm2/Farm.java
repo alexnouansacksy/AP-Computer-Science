04a3eb305d66cc9fa968ee68adba6340c5499c29
Alex Nouansacksy <nouansacks.a@stu.janesville.k12.wi.us>
2023-02-28 10:21:43 -0600

    Update

--- src/Farm2/Farm.java
+++ src/Farm2/Farm.java
@@ -8,36 +8,29 @@ public class Farm implements IFarm {
     private double myHayCost;
     private double myCornCost;
 
-    @Override
     public boolean feedAllAnimals() {
         return false;
     }
 
-    @Override
     public double farmIncome() {
         return 0;
     }
 
-    @Override
     public int getWeight() {
         return 0;
     }
 
-    @Override
     public double getCost() {
         return 0;
     }
 
-    @Override
     public ArrayList<Cow> getCows() {
         return null;
     }
 
-    @Override
     public ArrayList<Horse> getHorses() {
         return null;
     }
 
-    // …
 
 }
