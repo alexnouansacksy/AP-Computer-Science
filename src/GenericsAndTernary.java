052df952afb5392be375f50e171779f84d67f776
alexnouansacksy <nouansacks.a@stu.janesville.k12.wi.us>
2023-05-04 10:10:38 -0500

    Update

---
+++ src/GenericsAndTernary.java
@@ -0,0 +1,20 @@
+import static java.lang.System.out;
+public class GenericsAndTernary {
+    // Generic Typing
+    static class MyBox<T> {
+        private T myVal;
+        public MyBox (T thing) {myVal = thing;}
+        public void setVal(T thing) {myVal = thing;}
+        public T getVal() {return myVal;}
+    }
+
+    public static void main(String[] args) {
+        // ArrayList<TYPE> list = new ArrayList<TYPE>();
+        var box = new MyBox<Integer>(5);
+        String msg = (box.getVal() >= 5) ? "val >= 5" : "val < 5";
+        // Ternary Operator -> (condition) "if" ... "else" ...
+        out.println(msg);
+        box.setVal(10);
+        out.println(box.getVal());
+    }
+}
